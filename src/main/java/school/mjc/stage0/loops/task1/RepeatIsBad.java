5d559d26ed1e259417fe3af7193ae8489b23d3b7
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-31 20:41:50 +0400

    1 of 2 tests passed.

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,26 @@
 package school.mjc.stage0.loops.task1;
 
+import java.util.function.Supplier;
+
 public class RepeatIsBad {
 
-    @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    private static int count = 0;
 
+    public void repeatIsBad() {
+        Supplier<Void> action = () -> {
+            if (count < 20) {
+                System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+                count++;
+                repeatIsBad();
+            }
+            return null;
+        };
+        action.get();
+    }
+
+    public static void main(String[] args) {
+        RepeatIsBad repeatIsBad = new RepeatIsBad();
+        repeatIsBad.repeatIsBad();
+        System.out.println(count); // Output the final count
     }
 }
